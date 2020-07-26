9601dc7563a5bca02c1d10e1a621b88329b43034
unknown <jailson.alves@quicard.com.br>
2020-07-26 14:01:37 -0300

    procs

---
+++ instalacao_db/bkp_22_07_2020/sp_products_save.sql
@@ -0,0 +1,39 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_products_save`(
+pidproduct int(11),
+pdesproduct varchar(64),
+pvlprice decimal(10,2),
+pvlwidth decimal(10,2),
+pvlheight decimal(10,2),
+pvllength decimal(10,2),
+pvlweight decimal(10,2),
+pdesurl varchar(128)
+)
+BEGIN
+	
+	IF pidproduct > 0 THEN
+		
+		UPDATE tb_products
+        SET 
+			desproduct = pdesproduct,
+            vlprice = pvlprice,
+            vlwidth = pvlwidth,
+            vlheight = pvlheight,
+            vllength = pvllength,
+            vlweight = pvlweight,
+            desurl = CONVERT(pidproduct,CHAR)
+        WHERE idproduct = pidproduct;
+        
+    ELSE
+		
+		INSERT INTO tb_products (desproduct, vlprice, vlwidth, vlheight, vllength, vlweight, desurl) 
+        VALUES(pdesproduct, pvlprice, pvlwidth, pvlheight, pvllength, pvlweight, pdesurl);
+        
+        SET pidproduct = LAST_INSERT_ID();
+        
+        update tb_products set desurl=CONVERT(pidproduct,CHAR) where idproduct=pidproduct;
+        
+    END IF;
+    
+    SELECT * FROM tb_products WHERE idproduct = pidproduct;
+    
+END
---
+++ instalacao_db/bkp_22_07_2020/sp_userspasswordsrecoveries_create.sql
@@ -0,0 +1,13 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_userspasswordsrecoveries_create`(
+piduser INT,
+pdesip VARCHAR(45)
+)
+BEGIN
+  
+  INSERT INTO tb_userspasswordsrecoveries (iduser, desip)
+    VALUES(piduser, pdesip);
+    
+    SELECT * FROM tb_userspasswordsrecoveries
+    WHERE idrecovery = LAST_INSERT_ID();
+    
+END
